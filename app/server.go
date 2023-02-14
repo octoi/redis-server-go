a411b88412dbd1759ad3dd7d4198f4db909195a6
Fadhil <fadhilsaheer@gmail.com>
2023-02-14 19:29:56 +0530

    accepting multiple connections

--- app/server.go
+++ app/server.go
@@ -15,28 +15,33 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
+	for {
+		conn, err := l.Accept()
+		
+		if err != nil {
+			fmt.Println("error accepting connections: ", err.Error())
+			os.Exit(1)
+		}
 
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
 
 	for {
 		buf := make([]byte, 1024)
 
-		_, err := conn.Read(buf)
-
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			fmt.Println("Error reading from client: ", err.Error())
-			os.Exit(1)
+		if _, err := conn.Read(buf); err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println("error reading from client: ", err.Error())
+				os.Exit(1)
+			}
 		}
 
 		conn.Write([]byte("+PONG\r\n"))
 	}
-
 }
